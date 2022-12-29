4401396240555408eab1b7f1cd1982d30693e199
mateuszpyza <mateuszpyza1@wp.pl>
2022-12-29 22:00:31 +0100

    third task

--- src/main/java/models/ReadingData.java
+++ src/main/java/models/ReadingData.java
@@ -7,18 +7,41 @@ import java.nio.file.Path;
 
 public class ReadingData {
     public static void main(String[] args) {
-        reedFile();
+
+        writeEveryOtherLine(reedFile());
+
     }
 
-    public static void reedFile() {
+    public static String reedFile() {
         String dataPath = "C:\\Users\\pyzio\\OneDrive\\Pulpit\\Java_Projekty\\wyklad2Metody\\Files-Exceptions\\src\\main\\resources\\data.txt";
         File data = new File(dataPath);
         if (data.exists()) {
             try {
-                System.out.println(Files.readString(Path.of(dataPath)));
+                return Files.readString(Path.of(dataPath));
             } catch (IOException e) {
                 System.out.println("Problems with reading file");
             }
+        }
+        return "Problems with reading file";
+    }
+
+    public static void writeEveryOtherLine(String text) {
+        int startIndex = 0;
+        int endIndex = 0;
+        int counter = 0;
+        while (endIndex != -1) {
+            startIndex = text.indexOf("\n", startIndex);
+            endIndex = text.indexOf("\n", startIndex + 1);
+            if(endIndex==-1){
+                endIndex=text.length();
+                System.out.println(text.substring(startIndex, endIndex));
+                break;
+            }
+            if (counter % 2 == 0) {
+                System.out.println(text.substring(startIndex, endIndex + 1));
+            }
+            counter = counter + 1;
+            startIndex = startIndex + 1;
         }
     }
 }
