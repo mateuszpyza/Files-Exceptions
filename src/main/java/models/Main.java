7c0fc52e3e8d922010a1746b3d7cc7c49ec76721
mateuszpyza <mateuszpyza1@wp.pl>
2022-12-30 23:33:45 +0100

    fifth task first correction

--- src/main/java/models/Main.java
+++ src/main/java/models/Main.java
@@ -2,7 +2,6 @@ package models;
 
 public class Main {
     public static void main(String[] args) {
-
         boolean wordIsContain = WordInFile.containWord("C:\\Users\\pyzio\\OneDrive\\Pulpit\\Java_Projekty\\wyklad2Metody\\Files-Exceptions\\src\\main\\resources\\data.txt","Wyjątki");
         System.out.println(wordIsContain);
     }
