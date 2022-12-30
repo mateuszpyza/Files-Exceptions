ab153524715704259af8bf586cc7cab248f6bef2
mateuszpyza <mateuszpyza1@wp.pl>
2022-12-30 23:59:05 +0100

    second task first correction

--- src/main/java/models/Main.java
+++ src/main/java/models/Main.java
@@ -2,7 +2,8 @@ package models;
 
 public class Main {
     public static void main(String[] args) {
-        boolean wordIsContain = WordInFile.containWord("C:\\Users\\pyzio\\OneDrive\\Pulpit\\Java_Projekty\\wyklad2Metody\\Files-Exceptions\\src\\main\\resources\\data.txt","Wyjątki");
-        System.out.println(wordIsContain);
+        String fileContent = FileReader.reedFile("C:\\Users\\pyzio\\OneDrive\\Pulpit\\Java_Projekty\\wyklad2Metody\\Files-Exceptions\\src\\main\\resources\\data.txt");
+        System.out.println(fileContent);
+
     }
 }
