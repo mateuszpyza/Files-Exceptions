878c3edeacec9a332361321890bc7339b3a046a3
mateuszpyza <mateuszpyza1@wp.pl>
2022-12-30 23:52:47 +0100

    first task first correction

---
+++ src/main/java/apps/Main.java
@@ -0,0 +1,17 @@
+package apps;
+
+public class Main {
+    public static void main(String[] args){
+        FileSaver.createFileTxt("C:\\Users\\pyzio\\OneDrive\\Pulpit\\Java_Projekty\\wyklad2Metody\\Files-Exceptions\\src\\main\\resources\\data.txt","""
+                    Kurs
+                    Java
+                    Lekcja 6
+                    Pliki
+                    Wyjątki
+                    Pliki
+                    Koniec pliku""");
+
+    }
+
+
+}
