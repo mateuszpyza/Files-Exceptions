1b0bbf98771656cd873bed7cc2272d53f759a279
mateuszpyza <mateuszpyza1@wp.pl>
2022-12-30 21:50:00 +0100

    better?

--- src/main/java/apps/Main.java
+++ src/main/java/apps/Main.java
@@ -3,7 +3,11 @@ package apps;
 import exptions.MyCustomExceptions;
 
 public class Main {
-    public static void main(String []args) throws MyCustomExceptions {
-        SquareRoot.calculateSquareRoot(-8);
+    public static void main(String []args) {
+        try {
+            SquareRoot.calculateSquareRoot(-8);
+        }catch (MyCustomExceptions e){
+            System.out.println("IllegalArgumentException");
+        }
     }
 }
