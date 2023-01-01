0173bd174963720423d28335e74f0814f4e452d0
mateuszpyza <mateuszpyza1@wp.pl>
2023-01-01 11:50:54 +0100

    read,read,read

--- src/main/java/models/Main.java
+++ src/main/java/models/Main.java
@@ -2,7 +2,7 @@ package models;
 
 public class Main {
     public static void main(String[] args) {
-        String fileContent = FileReader.reedFile("C:\\Users\\pyzio\\OneDrive\\Pulpit\\Java_Projekty\\wyklad2Metody\\Files-Exceptions\\src\\main\\resources\\data.txt");
+        String fileContent = FileReader.readFile("C:\\Users\\pyzio\\OneDrive\\Pulpit\\Java_Projekty\\wyklad2Metody\\Files-Exceptions\\src\\main\\resources\\data.txt");
         System.out.println(fileContent);
 
     }
