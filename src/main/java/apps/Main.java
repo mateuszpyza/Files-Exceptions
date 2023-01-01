48ed3929165c7b4497f59b9c77b77abfd18095b9
mateuszpyza <mateuszpyza1@wp.pl>
2023-01-01 12:33:39 +0100

    better  solving

--- src/main/java/apps/Main.java
+++ src/main/java/apps/Main.java
@@ -4,6 +4,7 @@ import exeptions.MyCustomException;
 
 public class Main {
     public static void main(String[]args) throws MyCustomException {
-    PeselValidator.validatePesel("111111%1111");
+    PeselValidator.validatePesel("11111161111");
     }
+
 }
--- src/main/java/apps/PeselValidator.java
+++ src/main/java/apps/PeselValidator.java
@@ -5,22 +5,27 @@ import exeptions.MyCustomException;
 public class PeselValidator {
 
     public static void validatePesel(String pesel) throws MyCustomException {
-        if (pesel.length() != 11) {
-            throw new MyCustomException("Incorrect length PESEL");
+        try {
+            if (pesel.length() != 11) {
+                throw new MyCustomException("Incorrect length PESEL");
+            }
+            checkChar(pesel);
+        } catch (NullPointerException e) {
+            throw new MyCustomException("Input data problems");
         }
-        checkChar(pesel);
+
     }
 
     public static void checkChar(String pesel) throws MyCustomException {
         char[] arrayPesel = pesel.toCharArray();
         for (int i = 0; i <= pesel.length() - 1; i++) {
-            if (arrayPesel[i] != '0' && arrayPesel[i] != '1' && arrayPesel[i] != '2' &&
-                    arrayPesel[i] != '3' && arrayPesel[i] != '4' && arrayPesel[i] != '5' &&
-                    arrayPesel[i] != '6' && arrayPesel[i] != '7' && arrayPesel[i] != '8' &&
-                    arrayPesel[i] != '9') {
-                throw new MyCustomException("WrongTypeOfDataException");
+            try {
+                Integer.parseInt(String.valueOf(arrayPesel[i]));
+            } catch (NumberFormatException nfe) {
+                throw new MyCustomException("Input data problems");
             }
         }
     }
-
 }
+
+
