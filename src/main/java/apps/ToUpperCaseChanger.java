8a4093770f37632ceadd3c4b8559a90ee9f21d3a
mateuszpyza <mateuszpyza1@wp.pl>
2023-01-01 12:06:16 +0100

    read,read,read

--- src/main/java/apps/ToUpperCaseChanger.java
+++ src/main/java/apps/ToUpperCaseChanger.java
@@ -7,11 +7,11 @@ import java.nio.file.Path;
 
 public class ToUpperCaseChanger {
     public static void main(String[] args) {
-        reedToUpperCaseAndSave("C:\\Users\\pyzio\\OneDrive\\Pulpit\\Java_Projekty\\wyklad2Metody\\Files-Exceptions\\src\\main\\resources\\data.txt",
+        readToUpperCaseAndSave("C:\\Users\\pyzio\\OneDrive\\Pulpit\\Java_Projekty\\wyklad2Metody\\Files-Exceptions\\src\\main\\resources\\data.txt",
                 "C:\\Users\\pyzio\\OneDrive\\Pulpit\\Java_Projekty\\wyklad2Metody\\Files-Exceptions\\src\\main\\resources\\output.txt");
     }
 
-    public static boolean reedToUpperCaseAndSave(String pathToRead, String pathToSave) {
+    public static boolean readToUpperCaseAndSave(String pathToRead, String pathToSave) {
         return saveToFile(readFile(pathToRead).toUpperCase(), pathToSave);
     }
 
