bd4d26bdb952de58d4eed9f33fc778fe7ffe36dd
mateuszpyza <mateuszpyza1@wp.pl>
2023-01-01 12:42:52 +0100

    better  solving

--- src/main/java/apps/Main.java
+++ src/main/java/apps/Main.java
@@ -4,7 +4,7 @@ import exeptions.MyCustomException;
 
 public class Main {
     public static void main(String[]args) throws MyCustomException {
-    PeselValidator.validatePesel("11111161111");
+    PeselValidator.validatePesel("11111160111");
     }
 
 }
