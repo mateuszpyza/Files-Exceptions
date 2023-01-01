08bcef7eac8b6e826ba0a22d13542a5ae18fed4d
mateuszpyza <mateuszpyza1@wp.pl>
2023-01-01 11:55:47 +0100

    read,read,read

--- src/main/java/models/WordInFileContent.java
+++ src/main/java/models/WordInFileContent.java
@@ -3,7 +3,7 @@ package models;
 public class WordInFileContent {
     public static boolean containWord(String pathToFile, String word) {
         if (word != null) {
-            return FileReader.reedFile(pathToFile).contains(word);
+            return FileReader.readFile(pathToFile).contains(word);
         } else {
             throw new RuntimeException("Problems with input data!");
         }
